db9425b2ac6ddc010f66c482b57a8c1c507b7b4c
Mamuka Bzhalava <mbzhalava@mfa.ge>
2023-05-02 11:22:44 +0400

    errors

---
+++ auth/notes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    User = models.ForeignKey(User , related_name="notes", on_delete=models.CASCADE)
---
+++ auth/notes/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from rest_framework import Ap
+
+# Create your views here.
