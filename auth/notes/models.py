3fc4fc9d45fd3fb2e7b263c23bf52a2c6e002804
Mamuka Bzhalava <mbzhalava@mfa.ge>
2023-05-02 15:47:12 +0400

    saccess CRUD operation

--- auth/notes/models.py
+++ auth/notes/models.py
@@ -6,4 +6,4 @@ from django.contrib.auth.models import User
 class Note(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
-    User = models.ForeignKey(User , related_name="notes", on_delete=models.CASCADE)
+    user = models.ForeignKey(User , related_name="notes", on_delete=models.CASCADE)
--- auth/notes/views.py
+++ auth/notes/views.py
@@ -1,4 +1,73 @@
-from django.shortcuts import render
-from rest_framework import Ap
+from rest_framework.views import APIView
+from .serializers import NoteSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from .models import Note
+
+
 
 # Create your views here.
+class NoteCollectionView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = NoteSerializer
+
+    def get(self,request):
+        """List"""
+        notes = request.user.notes.all()
+        serializer = self.serializer_class(instance=notes, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+
+
+ 
+    def post(self,request):
+        """Create"""
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.user)
+
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+class NoteSingletoneView(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = NoteSerializer
+
+    def get(self, request, pk):
+        """Detail"""
+        note = get_object_or_404(Note, pk=pk)
+        serializer = self.serializer_class(instance=note)
+
+        return Response(serializer.data)
+
+
+
+
+
+    def patch(self,request,pk):
+        """Update"""
+        note = get_object_or_404(Note, pk=pk)
+
+        serializer = self.serializer_class(data=request.data, instance=note, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
+
+
+
+
+
+
+    def delete(self, request, pk):
+        """Delete"""
+        note = get_object_or_404(Note, pk=pk)
+
+        note.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
